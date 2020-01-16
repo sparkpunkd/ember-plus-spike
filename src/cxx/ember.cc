be7297a73a98c743058a317d76b6517756db43db
Richard Cartwright <spark@streampunk.media>
2020-01-16 11:23:16 +0000

    chore: sort of roundtrip decode

--- src/cxx/ember.cc
+++ src/cxx/ember.cc
@@ -7,6 +7,14 @@
 
 #define DECLARE_NAPI_METHOD(name, func) { name, 0, func, 0, 0, 0, napi_default, 0 }
 #define CHECK_STATUS if (checkStatus(env, status, __FILE__, __LINE__ - 1) != napi_ok) { return nullptr; }
+#define PASS_STATUS if (status != napi_ok) return status
+
+#define NAPI_THROW_ERROR(msg) { \
+  char errorMsg[256]; \
+  sprintf(errorMsg, "%s", msg); \
+  napi_throw_error(env, nullptr, errorMsg); \
+  return nullptr; \
+}
 
 napi_status checkStatus(napi_env env, napi_status status,
   const char* file, uint32_t line) {
@@ -76,7 +84,7 @@ napi_value testEncode (napi_env env, napi_callback_info info) {
 	char* gdd = new char[fred.size() + 1];
 	std::copy(fred.begin(), fred.end(), gdd);
 
-	status = napi_create_buffer_copy(env, fred.size() + 1, gdd, nullptr, &value);
+	status = napi_create_buffer_copy(env, fred.size(), gdd, nullptr, &value);
 	CHECK_STATUS;
 	status = napi_set_named_property(env, result, "asn1", value);
 	CHECK_STATUS;
@@ -182,6 +190,67 @@ napi_value tree (napi_env env, napi_callback_info info) {
 	return result;
 }
 
+void decodeCallback(
+	std::vector<unsigned char>::const_iterator first,
+	std::vector<unsigned char>::const_iterator last) {
+	printf("Decode callback has been called - length %i.\n", last - first);
+	std::vector<unsigned char>::const_iterator begin = first;
+	for ( auto it = first ; first != last ; first++ ) {
+		printf("%02X ", *first);
+	}
+	printf("\n");
+	first = begin;
+	libember::util::StreamBuffer<unsigned char> buf;
+	buf.append(first, last);
+
+	printf("Appended to buffer %i\n", buf.size());
+	buf.consume(9);
+	printf("Buffer after consume %i\n", buf.size());
+	libember::dom::DomReader reader;
+
+	printf("Created DOM reader\n");
+	libember::dom::Node* noddy = reader.decodeTree(buf, libember::glow::GlowNodeFactory::getFactory());
+	printf("I'm a node with encoded Length %i.\n", noddy->encodedLength());
+}
+
+napi_value testDecode (napi_env env, napi_callback_info info) {
+	napi_status status;
+	napi_value result, value;
+	bool testResult;
+	void* data;
+	size_t dataLength;
+
+	printf("Running testDecode\n");
+	napi_value argv[1];
+	size_t argc = 1;
+	status = napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+	CHECK_STATUS;
+
+	if (argc != 1) {
+		NAPI_THROW_ERROR("Test decoder must be provided with a single value - a buffer.");
+	}
+
+	status = napi_is_buffer(env, argv[0], &testResult);
+	CHECK_STATUS;
+	if (!testResult) {
+		NAPI_THROW_ERROR("Test decoder must be provided with a buffer.");
+	}
+
+	status = napi_get_buffer_info(env, argv[0], &data, &dataLength);
+	CHECK_STATUS;
+
+	char* datac = (char*) data;
+	std::vector<unsigned char> it(datac, datac + dataLength);
+
+	libs101::StreamDecoder<unsigned char> decoder;
+	decoder.read(it.begin(), it.end(), decodeCallback);
+
+	// Decode ASN.1 to Glow object
+	// Convert to something Javascripty
+
+	return nullptr;
+}
+
 napi_value version (napi_env env, napi_callback_info info) {
 	napi_status status;
 	napi_value result, value;
@@ -210,9 +279,10 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_property_descriptor desc[] = {
     DECLARE_NAPI_METHOD("testEncode", testEncode),
 		DECLARE_NAPI_METHOD("tree", tree),
-		DECLARE_NAPI_METHOD("version", version)
+		DECLARE_NAPI_METHOD("version", version),
+		DECLARE_NAPI_METHOD("testDecode", testDecode)
 	};
-  status = napi_define_properties(env, exports, 3, desc);
+  status = napi_define_properties(env, exports, 4, desc);
 	CHECK_STATUS;
 
   return exports;
